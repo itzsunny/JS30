4e85c955a58532a49d1211d9297b58462aa0f046
sunny <sunnychoudhury007@gmail.com>
2019-10-26 11:21:03 +0530

    add'

--- day2/public/js/main.js
+++ day2/public/js/main.js
@@ -1,15 +1,23 @@
 const getSeconds = document.querySelector(".second");
 const getMinutes = document.querySelector(".minute");
 const getHours = document.querySelector(".hour");
+const time = document.querySelector(".time");
 function setTime() {
   const date = new Date();
   const seconds = date.getSeconds();
   const minutes = date.getMinutes();
-  const secondDegrees = (((seconds / 60) * 360 )+ 90);
+  const hours = date.getHours();
+  const secondDegrees = (seconds / 60) * 360 + 90;
   getSeconds.style.transform = `rotate(${secondDegrees}deg)`;
-  const minuteDegrees = (((minutes / 60) * 360 )+ 90);
+  const minuteDegrees = (minutes / 60) * 360 + 90 + secondDegrees / 60;
   getMinutes.style.transform = `rotate(${minuteDegrees}deg)`;
-  const hourDegrees = (((minutes / 24) * 360 )+ 90);
+  const hourDegrees = (hours / 12) * 360 + 90 + minuteDegrees / 60;
   getHours.style.transform = `rotate(${hourDegrees}deg)`;
+  if (time.style.transform === `rotate(438deg)` || time.style.transform === `rotate(444deg)` ) {
+  time.classList.add('noTransition');
+  } else {
+  time.style.transition = `all 0.5s`;
+}
 }
 setInterval(setTime, 1000);
+setTime();
